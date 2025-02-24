1f7bb4ce634b2fd561e786f8b22d4f3e03e2e381
shiha13k <J.Krindrick13thF@gmail.com>
2025-02-24 16:22:54 +0300

    Create Calc

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    if (display.innerText === '0' && value !== '.') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+    if (display.innerText === '') {
+        display.innerText = '0';
+    }
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Ошибка';
+    }
+}
